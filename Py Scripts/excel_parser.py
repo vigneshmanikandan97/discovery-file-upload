c1a896ce87fa35a8ac09d0ca88ddab82f49e8a94
Vignesh Manikandan <vigneshm1797@gmail.com>
2019-05-20 15:56:44 +0530

    Updated parser

--- Py Scripts/excel_parser.py
+++ Py Scripts/excel_parser.py
@@ -2,50 +2,75 @@ from openpyxl import  load_workbook, Workbook
 from pathlib import Path
 import json
 
-# since format has only 4 columns by default
-COLS = 4
+def find_value_range(ws, row, col):
+    while ws.cell(row = row, column = col).value != None:
+        row += 1
+    return row - 1
 
-# Load workbook and worksheet
-wb = load_workbook(Path('D:\Vignesh Misc\code\discovery-file-upload\Assets\mock_data.xlsx'))
-ws = wb.get_sheet_by_name('Sheet1')
+def do_complex_parsing(ws, row, col, count, parsed_xl_json):
+    
+    return True
 
-# Get total records in the worksheet
-dimensions = ws.calculate_dimension()
-ROWS = int(dimensions.split('A1:D')[1])
-maxr = init_row = 0
-
-def get_maxr(a, b):
-    return a if a > b else b
-
-def xl_iterator(ROWS, COLS):
-    row_index = 3
+def xl_iterator(ws, ROWS, COLS):
+    parsed_xl_json = []
+    maxr = init_row = count = 0
     col_index = 1
-    parsed_xl_json = {}
+    row_index = 2
 
-    while row_index <= ROWS:
-        while col_index <= COLS:
-            if ws.cell(row = row_index, column = col_index).value == None:
-                maxr = get_maxr(maxr, row_index)
-                col_index += 1
-            else:
-                if col_index == 1:
-                    init_row = row_index
-                    parsed_xl_json[ws.cell(row = row_index, column = col_index).value] = {}
+    while col_index <= COLS:    
+        while row_index <= ROWS:
+            if ws.cell(row = row_index, column = col_index).value != None: # if current cell has value (not None)
+                if col_index == 1: # if current column is the serial number
+                    count += 1
+                    parsed_xl_json.append(dict({
+                        count: {}
+                        }))
+                    print(json.dumps(parsed_xl_json, indent = 4))
+                    init_row = maxr = row_index
+                    break
                 if col_index == 2:
-                    maxr = get_maxr(maxr, row_index)
+                    maxr = find_value_range(ws, row_index, col_index)
                     questions = ws.iter_cols(min_col = col_index, max_col = col_index, min_row = init_row, max_row = maxr, values_only = True)
                     for question in questions:
-                        print('Question: {}'.format(question))
+                        print('questions: {}'.format(question))
+                    row_index = maxr
                 if col_index == 3:
-                    maxr = get_maxr(maxr, row_index)
+                    maxr = find_value_range(ws, row_index, col_index)
                     sources = ws.iter_cols(min_col = col_index, max_col = col_index, min_row = init_row, max_row = maxr, values_only = True)
                     for source in sources:
-                        print('Sources: {}'.format(source))
+                        print('source: {}'.format(source))
+                    row_index = maxr
                 if col_index == 4:
-                    maxr = get_maxr(maxr, row_index)
+                    maxr = find_value_range(ws, row_index, col_index)
                     relevances = ws.iter_cols(min_col = col_index, max_col = col_index, min_row = init_row, max_row = maxr, values_only = True)
                     for relevance in relevances:
-                        print('Relevance: {}'.format(relevance))
+                        print('relevance: {}'.format(relevance))
+                    row_index = maxr
+                    col_index = 1 # some condition to be added so that infinite loop is avoided
                 row_index += 1
 
+            else:
+                """ 
+                if current cell has no value (is None)
+                    1. make row index as first index where data was found
+                    2. increment column index
+                    3. come out of row iteration
+                """
+                row_index = init_row
+                break
+        col_index += 1
+        print('Next up: {}r {}c'.format(row_index, col_index))
     return True
+
+if __name__ == "__main__":
+    # Load workbook and worksheet
+    wb = load_workbook(Path('C:/Users/manikvig/Documents/Work/discovery-file-upload/Assets/mock_data.xlsx'))
+    ws = wb['Sheet1']
+
+    # Get total records in the worksheet
+    dimensions = ws.calculate_dimension()
+    ROWS = int(dimensions.split('A1:D')[1])
+    # since format has only 4 columns by default
+    COLS = 4
+    
+    xl_iterator(ws, ROWS, COLS)
